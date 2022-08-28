f304bb8c25f718df99a6b6debf96d9abdcc4a227
Nikolai <nik-neman@mail.ru>
2022-08-28 16:43:26 +0200

    add jsp forms

--- src/main/java/com/nikolaikozlovskii/spring/mvc/MyController.java
+++ src/main/java/com/nikolaikozlovskii/spring/mvc/MyController.java
@@ -1,14 +1,56 @@
 package com.nikolaikozlovskii.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
+//@RequestMapping("/employee")//Добавляет employee перед каждым мапингом метода. Не обязателен.
 public class MyController {
 
     @RequestMapping("/")
     public String showFirstViev(){
-
         return "first_view";
+
+    }
+
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails(Model model){
+        model.addAttribute("employee", new Employee());
+        return "ask-emp-details-view";
+    }
+
+//    @RequestMapping("showDetails")
+//    public String showEmpDetails(){
+//        return "show-emp-details-view";
+//    }
+
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(HttpServletRequest request, Model model){
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr." + empName;
+//        model.addAttribute("nameAtribute", empName);
+//
+//        model.addAttribute("description", "- super Star");
+//
+//        return "show-emp-details-view";
+//    }
+
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model){
+//        empName = "Mr." + empName;
+//        model.addAttribute("nameAtribute", empName);
+//
+//        return "show-emp-details-view";
+//    }
+
+    @RequestMapping("/showDetails")
+    public String showEmpDetails(@ModelAttribute("employee") Employee emp){
+
+        return "show-emp-details-view";
     }
 }
--- src/main/webapp/WEB-INF/view/first_view.jsp
+++ src/main/webapp/WEB-INF/view/first_view.jsp
@@ -4,7 +4,15 @@
 <body>
 
 <h2>
-    I wish you luck in Spring MVC learning
+    I wish you luck in Spring MVC learning!!!
 </h2>
+<br>
+<br>
+
+<a href="askDetails">Please write your details</a>
+
+<%--<form action="askDetails.html" target="_blank">--%>
+<%--    <button>Next page</button>--%>
+</form>
 </body>
 </html>
